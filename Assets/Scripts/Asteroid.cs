ff27ed34baba367502bea761c95cdacb086c29dc
NathaFred <39423700+NathaFred@users.noreply.github.com>
2020-10-26 19:13:59 -0400

    Asteroid Prefab
    Added asteroid prefab, and script to control simple asteroid movement.

---
+++ Assets/Scripts/Asteroid.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Asteroid : MonoBehaviour
+{
+    public float rotateSpeed;
+    public float moveSpeed;
+    public Vector3 center;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //turn
+        transform.Rotate(Vector3.forward, rotateSpeed * Time.deltaTime);
+        transform.position += (transform.position - center).normalized * moveSpeed * Time.deltaTime;
+
+
+
+    }
+}
