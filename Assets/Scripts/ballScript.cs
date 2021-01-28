01c2b3d40f5d2ea3a01cfd03d52ded51374859ce
Perry Turner <38166274+cyberFobbit@users.noreply.github.com>
2021-01-27 23:47:11 -0500

    I did the ball!
    It still glitches a little bit, but it never changes speed, it loops in a sensical way (toroid), and drifts towards the center as it goes.

--- Assets/Scripts/ballScript.cs
+++ Assets/Scripts/ballScript.cs
@@ -2,34 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 [RequireComponent(typeof(Rigidbody))]
-public class ballScript : MonoBehaviour
+public class BallScript : MonoBehaviour
 {
     Camera cam;
     Rigidbody2D rb;
     public float speed;
+    public Vector3 center;
     // Start is called before the first frame update
     void Start()
     {
+        //set global variables
         cam = Camera.main;
         rb = GetComponent<Rigidbody2D>();
-        rb.AddForce(new Vector2(Random.value,Random.value) * speed, ForceMode2D.Impulse);
+
+
+        rb.AddForce(new Vector2(0,1) * speed, ForceMode2D.Impulse); //give the ball a swift kick to the pants
     }
 
     // Update is called once per frame
     void Update()
     {
-        //transform.position += (new Vector3(0.09f, 0.05f, 0)); //randomly move ball
-        
+        rb.velocity = speed * Vector3.Normalize(Vector2.Lerp(Vector3.Normalize(rb.velocity), -1 * Vector3.Normalize(transform.position), 0.5f * Time.deltaTime)); //maintain a constant speed and drift towards the center
         WrapBall(); //pretty self explanatory, iyam
     }
     
     void WrapBall(){
-        Vector3 destVp = new Vector3(0,0,cam.WorldToViewportPoint(transform.position).z); //Destination vecotr in Viewport space (easier to do math on)
+        Vector3 destVp = cam.WorldToViewportPoint(transform.position); //Destination vector in Viewport space (easier to do math on)
         if(cam.WorldToViewportPoint(transform.position).x > 1
-        || cam.WorldToViewportPoint(transform.position).x < 0
-        || cam.WorldToViewportPoint(transform.position).y > 1
-        || cam.WorldToViewportPoint(transform.position).y < 0){ //if out of bounds
+        || cam.WorldToViewportPoint(transform.position).x < 0){
             destVp.x = Mathf.Abs(cam.WorldToViewportPoint(transform.position).x - 1); //flippity
+            transform.position = cam.ViewportToWorldPoint(destVp); //your ball is my property
+        }
+        if(cam.WorldToViewportPoint(transform.position).y > 1
+        || cam.WorldToViewportPoint(transform.position).y < 0){ //if out of bounds
             destVp.y = Mathf.Abs(cam.WorldToViewportPoint(transform.position).y - 1); //floppity
             transform.position = cam.ViewportToWorldPoint(destVp); //your ball is my property
         }
